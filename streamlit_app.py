6b442a9d24ac2d654f2e144ed8abb84ead07a63a
vkumar008 <136806613+vkumar008@users.noreply.github.com>
2023-06-16 16:22:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit
 
-streamlit.title("My parents new healthy diner")
-streamlit.header("Menu")
-streamlit.text("CurdRice")
-streamlit.text("Juice")
-streamlit.text("RagiMalt")
+streamlit.title("My parents new healthy diner")  
+streamlit.header('Breakfast Menu')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(" 🍞CurdRice")
+streamlit.text(" 🥑 Juice")
+streamlit.text("🥣 RagiMalt")
 # streamlit.body("Hello")
 # print("Hello")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
