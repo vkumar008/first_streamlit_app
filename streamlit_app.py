15be7ca5ccd05414ec8db64a650dca7658b7bbec
vkumar008 <136806613+vkumar008@users.noreply.github.com>
2023-06-17 00:22:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,21 +49,22 @@ try:
                 streamlit.text("")
                 # fruits_added=streamlit.text_input("What fruit you would like to add ",         "Add Fruit",         key="placeholder",)
                 streamlit.text('Thanks for adding ',fruit_choice)
-my_cur.execute("insert into fruit_load_list values(' from streamlit')")
+                my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+                my_cur = my_cnx.cursor()
+                my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+                my_data_row = my_cur.fetchone()
+                streamlit.text("Hello from Snowflake:")
+                streamlit.text(my_data_row)
+
+                my_cur.execute("SELECT * from fruit_load_list")
+                my_data_rows = my_cur.fetchall()
+                streamlit.text("The fruit load list containts ")
+                streamlit.dataframe(my_data_rows)
+                my_cur.execute("insert into fruit_load_list values(' from streamlit')")
 except URLError as e:
         streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list containts ")
-streamlit.dataframe(my_data_rows)
 
 
 
